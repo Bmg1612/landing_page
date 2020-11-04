5ea7b893f0d0d966b41cce9e15e1ee98714a0fde
Bruno Gurgel <bmg1612@gmail.com>
2020-11-04 14:27:27 -0300

    fix: Refactored the code (part 2) ->
    - Added arrow functions;
    - Changed the 'getBoundingClientRect' function
      for the 'intersectionObserver' for performance purposes.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 // Global variables
 const navBar = document.querySelector("#navbar__list");
-const banner = document.querySelector(".banner__hero");
+const bannerMotives = document.querySelector(".motives__list__banner");
 const sections = document.getElementsByTagName("section");
-const button = document.querySelector("#banner__button");
 const footer = document.querySelector(".page__footer");
 const fragment = document.createDocumentFragment();
 
 
 // Function that creates dynamically the NavBar
-function createNavBar (event) {
+createNavBar = (event) => {
     for (let section of sections) {
         let newElement = document.createElement('li');
         newElement.innerHTML = `<a class="menu__link" id="menu__${section.id}" href="#${section.id}"> ${section.dataset.nav}</a>`;
@@ -23,8 +22,8 @@ const menuLink = document.querySelectorAll(".menu__link");
 const navList = document.querySelector('.navbar__menu');
 
 // Showing the sections content only when the linked Nav button is clicked
-function showSection () {
-    navList.addEventListener('click', function showContent (event) {
+showSection = () => {
+    navList.addEventListener('click', showContent = (event) => {
         event.preventDefault();
         for (let section of sections) {
             section.style.display = "none";
@@ -41,7 +40,7 @@ function showSection () {
 showSection();
 
 // Highlights the clicked menu item. Initially no item is highlighted
-function activeItem (){
+activeItem = () => {
     for (let item of menuLink) {
         item.addEventListener('click', function (event) {
             let current = event.target.id;
@@ -57,29 +56,33 @@ function activeItem (){
 }
 activeItem();
 
-// Helper function to see if an element is on the viewport
-function isInViewport(element) {
-    let bounding = element.getBoundingClientRect();
-    return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight) &&
-        bounding.right <= (window.innerWidth)
-    );
-};
-
 /*
-If the banner's button is on the viewport, no menu item is highlighted
+If the banner's motives list is on the viewport, no menu item is highlighted
 But if the user scrolls back to the section it gets highlighted again 
 */ 
-window.addEventListener('scroll', function highlightViewport() {
-    let activeItem = document.querySelector(".active");
-    activeItem.style.backgroundColor = isInViewport(button) ? "#ffc85a" : "";
-})
+observingEvent = () => {
+    let options = {
+        root: document.querySelector("#viewport"), //default
+        rootMargin: "0px", //default
+        threshold: 1.0
+    };
+    
+    let callback = (entries) => {
+        let activeItem = document.querySelector(".active");
+        entries.forEach((entry) => {
+            activeItem.style.backgroundColor = entry.isIntersecting ? "#ffc85a" : "";
+        });
+    };
+    
+    let observer = new IntersectionObserver(callback, options);
+    observer.observe(bannerMotives);  
+}
+observingEvent();
+
 
 // Function to display the menu list on Mobile & Tablet layouts    
 // Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon
-function toggleMenu(menu) {
+toggleMenu = (menu) => {
     // Menu animation
     menu.classList.toggle("change");
     if (navBar.style.display === "block") {
